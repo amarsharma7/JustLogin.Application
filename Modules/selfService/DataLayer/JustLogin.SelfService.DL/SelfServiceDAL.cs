be3ec11369cec688b24db4b52e3cb084e065fb4c
amar sharma <amardeep.sharma@sensationsolutions.com>
2019-01-29 11:10:54 +0530

    Milestone - 1
    . Create design screens (UI) according to the existing theme.
    2. Project setup and configuration and design implementation.
    3. Handler errors while connecting to the Fusebil according to the events as mentioned in the initianl document.

---
+++ Modules/selfService/DataLayer/JustLogin.SelfService.DL/SelfServiceDAL.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustLogin.SelfService.DL
+{
+    public class SelfServiceDAL
+    {
+        private static string LeaveDBConnStringName = "LeaveConnectionString";
+        private static string MembershipDBConnStringName = "MembershipDB";
+        private static string CustomizationDBConnStringName = "CustomizationDB";
+        
+
+        public static bool Company_CheckEmail(string Email)
+        {
+            bool flag = false;
+            using (SqlConnection dBConn = new SqlConnection(ConfigurationManager.ConnectionStrings[MembershipDBConnStringName].ConnectionString))
+            {
+                dBConn.Open();
+                using (SqlCommand cmd = new SqlCommand("SelfService_Company_CheckEmail", dBConn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@pEmail", Email));
+
+                    object obj = cmd.ExecuteScalar();
+                    
+                    if (obj != DBNull.Value)
+                    {
+                        flag = Convert.ToBoolean(obj);
+                    }
+                }
+            }
+            return flag;
+        }
+
+        public static int Company_UpdateInfo(string Email, string CompanyURL, string Password)
+        {
+            int rowCount = 0;
+            using (SqlConnection dBConn = new SqlConnection(ConfigurationManager.ConnectionStrings[MembershipDBConnStringName].ConnectionString))
+            {
+                dBConn.Open();
+                using (SqlCommand cmd = new SqlCommand("SelfService_Company_UpdateInfo", dBConn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@pEmail", Email));
+                    cmd.Parameters.Add(new SqlParameter("@pCompanyURL", CompanyURL));
+                    cmd.Parameters.Add(new SqlParameter("@pPassword", Password));
+                    rowCount = cmd.ExecuteNonQuery();
+                }
+            }
+            return rowCount;
+        }
+
+        public static string Company_Add(string Email, string CompanyName, string FullName, string PhoneNumber)
+        {
+            
+            string token = "";
+            using (SqlConnection dBConn = new SqlConnection(ConfigurationManager.ConnectionStrings[MembershipDBConnStringName].ConnectionString))
+            {
+                dBConn.Open();
+                using (SqlCommand cmd = new SqlCommand("SelfService_Company_Add", dBConn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@pEmail", Email));
+                    cmd.Parameters.Add(new SqlParameter("@pCompanyName", CompanyName));
+                    cmd.Parameters.Add(new SqlParameter("@pFullName", FullName));
+                    cmd.Parameters.Add(new SqlParameter("@pPhoneNumber", PhoneNumber));
+
+                    object obj = cmd.ExecuteScalar();
+                    if (obj != DBNull.Value)
+                    {
+                        token = obj.ToString();
+                    }
+                }
+            }
+            return token;
+        }
+    }
+}
---
+++ Modules/selfService/DataLayer/JustLogin.SelfService.DL/SelfServiceDataModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustLogin.SelfService.DL
+{
+    public enum SelfServiceStatus : byte
+    {
+        New = 1,
+        Verified=2,
+        URLUpdatedInDB=3,
+        CompanyCreated=4,
+        DNSCreated=5,
+        IISAliasCreated=6,
+        Demo=7,
+        ClearedDemoData=8,
+        Paid=9
+    }
+}
